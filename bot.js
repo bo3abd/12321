ca5f93a051ef52228ff291a592dca181904543bf
bo3abd <37047946+bo3abd@users.noreply.github.com>
2018-07-15 19:10:45 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,6 +7,29 @@ const YouTube = require('simple-youtube-api');
 const youtube = new YouTube("AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8");
 const queue = new Map();
 const client = new Discord.Client();
+///////////////////////////
+
+
+
+client.on("message", message => {
+  if (message.content.startsWith("#bc")) {
+               if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+let args = message.content.split(" ").slice(1);
+var argresult = args.join(' ');
+message.guild.members.filter(m => m.presence.status !== 'all').forEach(m => {
+m.send(`${argresult}\n ${m}`);
+})
+message.channel.send(`\`${message.guild.members.filter( m => m.presence.status !== 'all').size}\` عدد الأشخاص `);
+message.delete();
+};
+});
+
+
+
+
+
+
+
 
 //////////////////////-------------
 
