ec38a3252e848799b20fcdc79be4e5a65544b344
bo3abd <37047946+bo3abd@users.noreply.github.com>
2018-08-20 17:42:54 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,6 @@ const fetchVideoInfo = require('youtube-info');
 const YouTube = require('simple-youtube-api');
 const youtube = new YouTube("AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8");
 const queue = new Map();
-const Canvas = require("canvas")
 const client = new Discord.Client();
 const fs = require('fs');
 const moment = require('moment');
